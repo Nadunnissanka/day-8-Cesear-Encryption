bb9d311a9feb157a0eaa9f68f4e2a8896886be2d
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-06-11 14:37:33 +0000

    encode decode combine

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(art.logo+"\n\n")
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(t, s):
-    encrypted_msg = list()
-    for letter in t:
+
+def ceaser(start_text, shift_amount, cipher_direction):
+    end_msg = list()
+    for letter in start_text:
         position = alphabet.index(letter)
-        encrypted_position = position + s
+        if cipher_direction == "encode":
+            encrypted_position = position + shift_amount
+        elif cipher_direction == "decode":
+            encrypted_position = position - shift_amount
         encrypted_letter = alphabet[encrypted_position]
-        encrypted_msg.append(encrypted_letter)
-    encrypted_output = ''.join(encrypted_msg)
-    print(encrypted_output)    
+        end_msg.append(encrypted_letter)
+    encrypted_output = ''.join(end_msg)
+    print(f"\nThe {cipher_direction} message is:   {encrypted_output}\n")
 
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text, shift)
+ceaser(start_text = text, shift_amount = shift, cipher_direction = direction)
 
 
